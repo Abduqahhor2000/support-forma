4099c420f09680d8ab0d79185d315eeb66a118bf
AbduqahhorNorimmatov <AbduqahhorDeveloper@gmail.com>
2022-10-21 10:30:56 +0500

    push

--- src/Containers/Career.js
+++ src/Containers/Career.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
 import BgMap from "../img/bg_map.png";
-import CareerBanner from "../img/career_banner.png"
+import CareerBanner from "../img/career_banner.png";
+import { true_input } from "../img/svg";
 
 export default function Career() {
+  const [inputTrue, setInputTrue] = useState(false);
   return (
     <div>
       <div className="min-h-[700px] bg-color-brand relative flex items-center justify-center">
@@ -21,10 +23,46 @@ export default function Career() {
               inquiring minds, and a strong desire for self-perfection and
               continuous growth.
             </p>
-            <span className="py-2.5 px-20 bg-gradient-to-l tracking-widest from-gradient-color-1 to-gradient-color-2 rounded-full cursor-pointer hover:bg-gradient-to-r">EXPLORE ROLES</span>
+            <span className="py-2.5 px-20 bg-gradient-to-l tracking-widest from-gradient-color-1 to-gradient-color-2 rounded-full cursor-pointer hover:bg-gradient-to-r">
+              EXPLORE ROLES
+            </span>
           </div>
           <div className="w-[450px] h-[385px]">
-            <img className="w-full h-full block" src={CareerBanner} alt=""></img>
+            <img
+              className="w-full h-full block"
+              src={CareerBanner}
+              alt=""
+            ></img>
+          </div>
+        </div>
+      </div>
+      <div className="min-h-[600px] flex">
+        <div>
+          <h2 className="w-[470px] text-[40px] font-semibold">
+            Let’s discuss how can we help you
+          </h2>
+        </div>
+        <div className="relative">
+          <div className="absolute w-[630px] shadow-cardJob rounded-lg p-8">
+            <h3 className="text-3xl text-center tracking-wider font-semibold">
+              Our Jobs
+            </h3>
+            <div className="flex flex-wrap justify-between">
+              <div className="relative">
+                <input type="text" required className={`peer border-input-border border-[1px] rounded-3xl border-solid h-11 w-[275px] outline-none focus:border-input-focus duration-150 pl-4 pr-10 ${inputTrue ? "border-input-succes": "border-input"}`} />
+                <span className="absolute top-3 left-4 peer-focus:-top-2 peer-valid:-top-2 bg-white px-0.5 text-sm text-placeholder duration-200 pointer-events-none">
+                  Full name*
+                </span>
+                <span className="absolute peer-focus:opacity-0 top-2.5 duration-200 right-3">{true_input}</span>
+              </div>
+              <input type="tel" />
+              <input type="email" />
+              <select>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+              </select>
+            </div>
           </div>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,15 @@ module.exports = {
         'color-brand': 'rgba(0, 39, 105, 1)',
         'gradient-color-1': "#069E81",
         'gradient-color-2': "#29DEBB",
+        "input-border": "rgba(227, 227, 227, 1)",
+        "input-focus": "rgba(51, 137, 233, 1)",
+        "input-succes": "rgba(39, 174, 96, 1)",
+        "input-error": "rgba(216, 59, 59, 1)",
+        "placeholder": "rgba(0, 30, 80, 0.5)",
       },
+      boxShadow: {
+        "cardJob": "0px 4px 20px rgba(0, 0, 0, 0.2)",
+      }
     },
   },
   plugins: [],
